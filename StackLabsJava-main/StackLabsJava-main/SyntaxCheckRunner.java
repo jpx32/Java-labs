6253ac114a696df7cb928f83835827080598d635
jpx32 <108827236+jpx32@users.noreply.github.com>
2022-07-08 02:37:27 +0000

    Add files via upload

---
+++ StackLabsJava-main/StackLabsJava-main/SyntaxCheckRunner.java
@@ -0,0 +1,22 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Jeremiah Pitts
+
+import java.util.Stack;
+import static java.lang.System.*;
+
+public class SyntaxCheckRunner
+{
+	public static void main ( String[] args )
+	{
+		SyntaxChecker one = new SyntaxChecker("(abc(*def)") ;
+        SyntaxChecker two = new SyntaxChecker("[{}]");
+        SyntaxChecker three = new SyntaxChecker( "[");	
+        SyntaxChecker four = new SyntaxChecker("[{<()>}]") ;
+        SyntaxChecker five = new SyntaxChecker("{<html[value=4]*(12)>{$x}}") ;
+        SyntaxChecker six = new SyntaxChecker("[one]<two>{three}(four)") ;
+        SyntaxChecker seven = new SyntaxChecker("car(cdr(a)(b)))") ;
+        SyntaxChecker eight = new SyntaxChecker("car(cdr(a)(b))") ;
+	}
+}
